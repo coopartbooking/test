0f56e7cc9971cfc96df347642eba3a037bd9e262
coopartbooking <commercial@coopart.fr>
2026-04-13 17:50:47 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -906,6 +906,16 @@ async removeGlobalTag(familyName, tag) {
             }
         },
 
+        goToContactStructure(c) {
+            const parentStruct = this.db.structures.find(s => s.id === c.structId);
+            if (parentStruct) {
+                this.tab = 'structures';
+                this.openCrmView(parentStruct);
+            } else {
+                Swal.fire({ title: 'Structure introuvable', text: 'La structure liée à ce contact est introuvable dans la base.', icon: 'warning', toast: true, position: 'top-end', timer: 3000, showConfirmButton: false });
+            }
+        },
+
         deleteContact(c) {
             Swal.fire({ title: 'Supprimer ce contact ?', icon: 'warning', showCancelButton: true, confirmButtonColor: '#ef4444' })
                 .then(r => { if (r.isConfirmed) { const ps = this.db.structures.find(s => s.id === c.structId); if (ps) { ps.contacts = ps.contacts.filter(x => x.id !== c.id); this.saveDB(); } } });
