aa42fb11397f5332cf5c171440658e08d9ec30d5
coopartbooking <commercial@coopart.fr>
2026-07-24 08:22:30 +0200

    Add files via upload

--- js/modules/crmMethods.js
+++ js/modules/crmMethods.js
@@ -2,7 +2,7 @@
 // Section : entre // --- MOTEUR CRM --- et // --- ADMIN ---
 // Note : nextTick() remplacé par this.$nextTick() (équivalent dans le contexte composant Vue)
 
-import { matchStructure, addAlias, scanDuplicates, markNotDuplicate, mergeInto, buildMergeComment } from './structMatch.js?v=19';
+import { matchStructure, addAlias, scanDuplicates, markNotDuplicate, mergeInto, buildMergeComment } from './structMatch.js?v=20';
 
 export const crmMethods = {
 
